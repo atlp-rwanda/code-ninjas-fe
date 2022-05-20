6e6a4dcbf8a7db7720aae252a9373524163fff8d
itisWasp <mugishaisrael18@gmail.com>
2022-05-20 11:57:50 +0200

    182234874 (Feature): Implement landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home';
+import Home from './pages/index';
 import Welcome from './components/Welcome';
 import AboutPage from './components/about/About';
 
---
+++ src/components/home/AppBar.jsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import AppBar from './AppBarLayout';
+import Toolbar from './ToolBar';
+import Theme from '../../styles/muiTheme';
+
+const rightLink = {
+  fontSize: 16,
+  color: 'common.white',
+  ml: 3,
+};
+
+function AppAppBar() {
+  return (
+    <div>
+      <AppBar position="fixed" sx={{ bgcolor: Theme.bgcolor }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ flex: 1 }} />
+          <Link
+            variant="h6"
+            underline="none"
+            color="inherit"
+            href="/"
+            sx={{ fontSize: 24 }}
+          >
+            Barefoot Nomad
+          </Link>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              href="/"
+              sx={rightLink}
+            >
+              <Button
+                sx={{ color: 'common.white' }}
+                color="secondary"
+                variant="outlined"
+              >
+                Login
+              </Button>
+            </Link>
+            <Link
+              variant="h6"
+              underline="none"
+              href="/"
+              sx={{ ...rightLink, color: 'common.white' }}
+            >
+              <Button
+                sx={{ color: 'common.white' }}
+                color="secondary"
+                variant="outlined"
+              >
+                Register
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </div>
+  );
+}
+
+export default AppAppBar;
